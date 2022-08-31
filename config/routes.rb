748136eb21744592b4055c69be7de77aa4587163
shinya524 <gheena1800@gmail.com>
2022-08-31 02:30:32 +0000

    [Add]6-3 非同期通信化

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about",as: 'about'
-  get 'searches/search'=>'searches#search',as: 'search'
+  get '/search'=>'searches#search',as: 'search'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
